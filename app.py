4267051d9673e5c8aa5bebeda0d6191ae0e65d0a
arpitGuptaSX <arpit@sqrx.com>
2025-03-13 16:04:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ flow = Flow.from_client_secrets_file(
         "openid",
         "https://www.googleapis.com/auth/drive"  # Full Drive access scope 
     ],
-    redirect_uri="https://marketingviaai.onrender.com/callback"
+    redirect_uri="https://app-diagrams-net.onrender.com/callback"
 )
 
 @app.route("/")
